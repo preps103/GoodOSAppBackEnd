4396a0365b46c718e9da3833f31cb70c9d057468
Maurice <maurice@Maurices-iMac.local>
2026-07-22 07:21:15 -0700

    Align Android SDK JVM toolchain

--- sdks/kotlin/build.gradle.kts
+++ sdks/kotlin/build.gradle.kts
@@ -10,10 +10,16 @@ version = "0.1.0"
 android {
     namespace = "app.goodos.goodbase"
     compileSdk = 35
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
     defaultConfig { minSdk = 26; consumerProguardFiles("consumer-rules.pro"); externalNativeBuild { cmake { cppFlags += "-std=c++17" } } }
     externalNativeBuild { cmake { path = file("src/main/cpp/CMakeLists.txt"); version = "3.22.1" } }
     publishing { singleVariant("release") { withSourcesJar() } }
 }
+
+kotlin { jvmToolchain(17) }
 
 dependencies { implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1") }
 
